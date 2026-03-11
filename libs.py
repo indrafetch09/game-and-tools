6e309bd9367f5915700f3a4680489c0cfbd06288
indrafetch09 <harindrawahyu131@gmail.com>
2026-03-11 15:35:34 +0700

    remove unused comment on libs.py

--- libs.py
+++ libs.py
@@ -1,29 +1,31 @@
-# FUNCTION
 import socket
 from time import sleep
 
 PC_NAME = socket.gethostname()
 
+
 def welcome_massage():
     style = "*" * (len(PC_NAME) + 14)
-    
+
     print(style)
     print(f"** WELCOME {PC_NAME} **")
     print(style)
-    
+
+
 def exit_program():
-    print('Program akan dihentikan')
+    print("Program akan dihentikan")
     sleep(1)
-    print('3...')
+    print("3...")
     sleep(1)
-    print('2..')
+    print("2..")
     sleep(1)
-    print('1..') 
+    print("1..")
     sleep(1)
-    print('Program berhasil dihentikan')
+    print("Program berhasil dihentikan")
     print("\nTerima kasih telah menggunakan program ini 👋")
     exit()
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     welcome_massage()
     exit_program()
